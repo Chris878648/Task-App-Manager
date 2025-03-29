383d77bfa2e135b6caef9c0de34fe4136b48d1c1
Chris878648 <134880684+Chris878648@users.noreply.github.com>
2025-03-29 01:00:40 -0600

    Update MainLayouts.jsx

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -102,6 +102,7 @@ const MainLayout = ({ children }) => {
     try {
       await logout(); 
       localStorage.removeItem("token");
+      toast.success('Login successful!');
       navigate("/login");
     } catch (error) {
       console.error("Error logging out:", error);
